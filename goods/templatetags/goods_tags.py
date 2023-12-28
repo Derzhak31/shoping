cfbec1ffedb8eb9ab3dbae4cae7789a00c576636
Derzhak31 <derzhak31@gmail.com>
2023-12-28 15:33:40 +0300

    first commit

---
+++ goods/templatetags/goods_tags.py
@@ -0,0 +1,23 @@
+from django import template
+from django.utils.http import urlencode
+
+from goods.models import Categories, Products
+
+register = template.Library()
+
+
+@register.simple_tag()
+def tag_products():
+    return Products.objects.all()
+
+
+@register.simple_tag()
+def tag_categories():
+    return Categories.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
